f3c7ef46916cd4c6a000eee1238d3b53bcc279f6
zubairahmedi <zkzubairahmedi@gmail.com>
2025-09-04 13:28:44 -0400

    Feat: Increase payload size limit to 50mb

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // Route: split string into 10 parts
 app.post("/split", (req, res) => {
